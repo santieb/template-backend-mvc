04fdf61c8f79b0faf6ca4dca49aa0493c245d55b
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-18 23:16:33 -0300

    add: responses

---
+++ src/shared/helpers/sucessResponse.ts
@@ -0,0 +1,20 @@
+import { Response } from 'express'
+
+type response = {
+  res: Response
+  status: number
+  message: string
+  token?: string
+  data?: any
+}
+
+const sucessResponse = ({ res, status = 200, message, data, token } : response) => {
+  res.status(status).json({
+    status,
+    message,
+    data,
+    token
+  })
+}
+
+export default sucessResponse
